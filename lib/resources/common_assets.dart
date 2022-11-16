b48bfec0366e6d7c7b1cb90fa486b0b79e9f9879
Sahil Garg <sahilg@geekyants.com>
2022-11-16 11:53:24 +0530

    Added the svg for logo and login page

--- lib/resources/common_assets.dart
+++ lib/resources/common_assets.dart
@@ -1,5 +1,6 @@
 class CommonAssets {
-  // static const String introVideo0 = '';//Add link to video here
   static const String splashImage = 'assets/images/moving_boxes.svg';
   static const String loginImage = 'assets/images/two_person_working.svg';
+  static const String twoPersonGroupImage = 'assets/images/group_working.svg';
+  static const String logoPilog = 'assets/images/logo_pilog.svg';
 }

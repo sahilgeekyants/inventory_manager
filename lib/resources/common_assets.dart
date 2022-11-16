901022ebcb5589664ca61459dc50f73c5345d163
Sahil Garg <sahilg@geekyants.com>
2022-11-16 18:11:55 +0530

    Added basic UI for splash page and other changes

--- lib/resources/common_assets.dart
+++ lib/resources/common_assets.dart
@@ -2,6 +2,7 @@ class CommonAssets {
   static const String splashImage = 'assets/images/moving_boxes.svg';
   static const String loginImage = 'assets/images/two_person_working.svg';
   static const String twoPersonGroupImage = 'assets/images/group_working.svg';
+  static const String twoPersonGroupPngImage = 'assets/images/group_working_png.png';
   static const String logoPilog = 'assets/images/logo_pilog.svg';
   static const String logoPilogPng = 'assets/images/logo_pilog.png';
 }

24af620c5eabfeff0f4b0c49288668223b737c6a
Vikrant Singh <vsc.uiet@gmail.com>
2022-11-18 10:58:58 +0530

    Designed the bottom modal UI partially

--- lib/resources/common_colors.dart
+++ lib/resources/common_colors.dart
@@ -11,4 +11,8 @@ class CommonColors {
   static const Color kTableBorderColor = Color(0xffE8E8E8);
   static const Color kBlackShadowColor = Color(0x30000000);
   static const Color kBlackIconColor = Color(0xff000000);
+  static const Color kTransparentColor = Color(0x00ffffff);
+  static const Color kTextFieldColor = Color(0xff9d9d9d);
+  static const Color kSearchButtonColor = Color(0xffA0ABC0);
+  static const Color kBorderLightGrey = Color(0xffE3E3E3);
 }
